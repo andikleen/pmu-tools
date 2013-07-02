df1f026164aa11bc1cd30159919a34fa498eb84f
Andi Kleen <ak@linux.intel.com>
2013-07-02 10:18:15 -0700

    interval-plot: use multiple sub plots for different levels

--- interval-plot.py
+++ interval-plot.py
@@ -9,13 +9,15 @@
 import csv
 import sys
 import matplotlib.pyplot as plt
+import collections
 
-# currently need 39 colors max. we reuse after 20 XXX
+import gen_level
+
 all_colors = ('silver','grey','black','orange','brown','maroon',
 	      'green','olive','purple','red', '#6960EC', '#F0FFFF',
               '#728C00', '#827B60', '#F87217', '#E55451', # 16
               '#F88017', '#C11B17', '#17BFC2', '#C48793') # 20
-cur_color = all_colors
+cur_colors = collections.defaultdict(lambda: all_colors)
 assigned = dict()
 
 if len(sys.argv) > 1:
@@ -33,10 +35,12 @@ for r in rc:
         continue
     event = r[1]
     if event not in assigned:
-        assigned[event] = cur_color[0]		
-        cur_color = cur_color[1:]
-        if len(cur_color) == 0:
-            cur_color = all_colors
+        level = gen_level.get_level(event)
+        print level, event
+        assigned[event] = cur_colors[level][0]
+        cur_colors[level] = cur_colors[level][1:]
+        if len(cur_colors[level]) == 0:
+            cur_colors[level] = all_colors
         value[event] = []
         timestamps[event] = []
     timestamps[event].append(r[0])
@@ -45,13 +49,20 @@ for r in rc:
     except ValueError:
         value[event].append(0.0)
 
-fig, ax = plt.subplots(1)
-ax.set_ylim(0, 100)
-t = []
+levels = dict()
 for j in assigned.keys():
-    t.append(j)
-    ax.plot(timestamps[j], value[j], assigned[j])
-leg = ax.legend(t, loc='upper left')
-leg.get_frame().set_alpha(0.5)
-plt.show()
+    levels[gen_level.get_level(j)] = True
 
+n = 1
+for l in levels.keys():
+    ax = plt.subplot(len(levels), 1, n)
+    ax.set_ylim(0, 100)
+    t = []
+    for j in assigned.keys():
+        if gen_level.get_level(j) == l:
+            t.append(j)
+            ax.plot(timestamps[j], value[j], assigned[j])
+    leg = ax.legend(t, loc='upper left')
+    leg.get_frame().set_alpha(0.5)
+    n += 1
+plt.show()
