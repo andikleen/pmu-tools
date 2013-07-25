6a368df2f17ad8d7c03807df3b79cfd0557c1318
Andi Kleen <ak@linux.intel.com>
2013-07-24 18:36:20 -0700

    add missing gen_level module

---
+++ gen_level.py
@@ -0,0 +1,20 @@
+# generate levels for events from the model
+# utility module for other tools
+# the model used must be a superset of all
+import ivb_server_ratios
+
+omap = dict()
+
+class Runner:
+    def run(self, n):
+        #print "name |%s|" % (n.name), n
+        omap[n.name] = n
+
+def get_level(name):
+    name = name.strip()
+    if name in omap:
+        return omap[name].level
+    return 0
+
+runner = Runner()
+ivb_client_ratios.Setup(runner)
