a5715341fe891ac5be35a98f7382ef2494490b20
Andi Kleen <ak@linux.intel.com>
2013-11-10 10:40:38 -0800

    parser: Initial converter from perf.data to pandas DataFrame
    This allows to do pandas/scipy style data mining on perf.data
    
    Still only a subset of the perf data properly supported

---
+++ parser/perfpd.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# Import perf.data into a pandas DataFrame
+# May need substantial memory for large input files.
+import pandas as pd
+import numpy as np
+import perfdata
+from collections import defaultdict
+
+#
+# TBD 
+# resolve ips
+# extra table for threads
+# good data model for callchains and branch_stack
+# expand registers, stack
+# represent other metadata
+# s/w trace points
+# 
+
+def samples_to_df(h):
+    ev = perfdata.get_events(h)
+    index = []
+    data = defaultdict(list)
+    for j in ev:
+        if j.type != "SAMPLE":
+            continue
+        for name in j:
+            if name.startswith("__"):
+                continue
+            data[name].append(j[name])
+        # XXX assumes time exists
+        index.append(j["time"])
+    print "index", len(index)
+    return pd.DataFrame(data, index=index, dtype=np.uint64)
+
+def read_samples(fn):
+    with open(fn, "rb") as f:
+        h = perfdata.perf_file.parse_stream(f)
+        df = samples_to_df(h)
+        return df
+
+if __name__ == '__main__':
+    import sys
+    df = read_samples(sys.argv[1])
+    print df
