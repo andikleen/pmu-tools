7c7236ddda0f8ba7242ccc9f0554155a8a561456
Andi Kleen <ak@linux.intel.com>
2013-12-06 19:57:39 -0800

    parser: add simple hist utility to print histogram

---
+++ parser/hist.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# print histogram for perf.data
+import perfpd
+import argparse
+
+p = argparse.ArgumentParser(description='Print histogram for perf.data')
+p.add_argument('datafiles', nargs='*', 
+               help='perf.data files (default perf.data)',
+               default=['perf.data'])
+p.add_argument('--sort', help='field to sort on (symbol, line)', 
+               default='symbol')
+p.add_argument('--min-percent', help='Minimum percent to print', default=0.0)
+args = p.parse_args()
+
+def print_feat(feat):
+    print "# Measured on %s (%s)" % (
+            feat.hostname.hostname,
+            feat.osrelease.osrelease)
+    print "# %s, %s" % (
+            feat.cpudesc.cpudesc,
+            feat.cpuid.cpuid)
+    print "# %s" % (" ".join(map(lambda x: x.cmdline, feat.cmdline.cmdline)))
+
+min_percent = float(args.min_percent) / 100.0
+for d in args.datafiles:
+    df, et, feat = perfpd.read_samples(d)
+    h = df[args.sort].value_counts(normalize=True)
+    cols = min(max(map(lambda x: len(x), h.index)) + 5, 70)
+    print_feat(feat)
+    for s, v in zip(h.index, h.values):
+       if v < min_percent:
+           break
+       print "%-*s %.2f%%" % (cols, s, v*100.0)
