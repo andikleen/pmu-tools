982e761d126a55a24cd7512090049d3f9c9065b3
Andi Kleen <ak@linux.intel.com>
2013-12-29 20:43:43 -0800

    tl-barplot:  improvements
    use argument parser
    use brewer colors
    add axis description
    truncate time stamp

--- tl-barplot.py
+++ tl-barplot.py
@@ -3,22 +3,34 @@
 # works best with -v and -l1
 import matplotlib.pyplot as plt
 import csv
-import sys
+import argparse
+import math
 from collections import defaultdict
 
-all_colors = ('red','green','blue','yellow','black')
+try:
+    import brewer2mpl
+    all_colors = brewer2mpl.get_map('Spectral', 'Diverging', 4).hex_colors
+except ImportError:
+    print "pip install brewer2mpl for better colors"
+    all_colors = ('red','green','blue','yellow','black')
 
 ratios = defaultdict(list)
 timestamps = []
 
-rc = csv.reader(open(sys.argv[1], "r"))
+args = argparse.ArgumentParser(usage='plot toplev -l1 -v -x, output as bar plot') 
+args.add_argument('file', help='CSV file to plot')
+p = args.parse_args()
+
+rc = csv.reader(open(p.file, "r"))
 ts = None
 for r in rc:
     if len(r) < 4:
         continue
-    if r[0] != ts:
-        timestamps.append(float(r[0]))
-        ts = r[0]
+    t = math.trunc(float(r[0]) * 100) / 100.0
+    print t,r[1]
+    if t != ts:
+        timestamps.append(t)
+        ts = t
     ratios[r[1]].append(float(r[2].replace("%","")))
     
 bars = []
@@ -31,6 +43,8 @@ for j in ratios:
     col = col[1:]
     if not col:
         col = all_colors
-                
+               
+plt.xlabel('Time')
+plt.ylabel('Percent bottleneck')
 plt.legend(map(lambda x: x[0], bars), ratios.keys())
 plt.show()
