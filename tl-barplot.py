892a45400097ad86e9019f55180cd5e2470c1392
Andi Kleen <ak@linux.intel.com>
2013-12-30 19:21:09 -0800

    tl-barplot: handle multiple levels (still some issues)

--- tl-barplot.py
+++ tl-barplot.py
@@ -32,12 +32,14 @@ for r in rc:
     if not re.match(r"\d+(\.\d*)", r[0]):
         r = ["0.0"] + r
     l = gen_level.get_level(r[1])
+    #print r[1], l
     if r[1] not in levels[l]:
         levels[l].append(r[1])
     t = math.trunc(float(r[0]) * 100) / 100.0
     if t != ts:
         timestamps.append(t)
         ts = t
+    # xxx fill in dummy values if not using -v
     ratios[r[1]].append(float(r[2].replace("%","")))
 
 print "time", len(timestamps), timestamps
@@ -45,22 +47,30 @@ for j in ratios.keys():
     print j, ratios[j]
     
 n = 0
+numplots = len(levels.keys())
 fig = plt.figure()
 for l in levels.keys():
-    ax = fig.add_subplot(len(levels.keys()), 1, n)
-    r = map(lambda x: ratios[x], levels[l])
-    print levels[l]
-    print r
+    non_null = filter(lambda x: sum(ratios[x]) != 0.0, levels[l])
+    if not non_null:
+        print "nothing in level", l
+        n += 1
+        continue
+    ax = fig.add_subplot(numplots, 1, n)
+    r = map(lambda x: ratios[x], non_null)
     stack =  ax.stackplot(timestamps, colors=all_colors, *r)
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Bottleneck (% of execution time)')
+    ax.set_ylim(0, 100)
+    ax.set_title('Level %d' % (l))
 
     p = [plt.Rectangle((0, 0), 1, 1, fc=pc.get_facecolor()[0]) for pc in stack]
-    leg = plt.legend(p, levels[l])
+    leg = plt.legend(p, non_null)
     leg.get_frame().set_alpha(0.5)
     ax.margins(0, 0)
     n += 1
 
 if len(timestamps) == 1:
     plt.gca().axes.get_xaxis().set_visible(False)
+
+# xxx put in wrong place
+#plt.xlabel('Time (s)')
+#plt.ylabel('Bottleneck (% of execution time)')
 plt.show()
