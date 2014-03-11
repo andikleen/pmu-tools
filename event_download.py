08b4421e9b73e9240fd0f076b70e96fbe7ac86f4
Andi Kleen <ak@linux.intel.com>
2014-03-10 18:09:46 -0700

    fix event_download

--- event_download.py
+++ event_download.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # automatically download json event files
 #
-# event-download.py         download for current cpu
-# event-download.py -a      download all
-# event-download.py cpustr...  Download for specific CPU
+# event_download.py         download for current cpu
+# event_download.py -a      download all
+# event_download.py cpustr...  Download for specific CPU
 import sys
 from urllib2 import urlopen, URLError
 import argparse
@@ -11,7 +11,7 @@ import os
 import string
 from fnmatch import fnmatch
 
-urlpath = 'http://laut.jf.intel.com/events'
+urlpath = 'https://download.01.org/perfmon'
 mapfile = 'mapfile.csv'
 modelpath = urlpath + "/" + mapfile
 
@@ -46,24 +46,29 @@ def dodir():
     except OSError:
         sys.exit('Cannot access ~/.events')
 
-def download(match):
+def getfile(url, fn):
+    print "Downloading", url, "to", fn
+    f = urlopen(url)
+    o = open(fn, "w")
+    o.write(f.read())
+    o.close()
+    f.close()
+
+def download(match, key="core"):
     found = 0
     try:
+        print "Downloading", modelpath
         models = urlopen(modelpath)
         for j in models:
-            cpu, version, name  = j.rstrip().split(",")
-            if not fnmatch(cpu, match):
+            cpu, version, name, type  = j.rstrip().split(",")
+            if not fnmatch(cpu, match) or type != key:
                 continue
             cpu = sanitize(cpu, string.ascii_letters + '-' + string.digits)
             url = urlpath + name
-            print "Downloading", url, "to", cpu + ".json"
-            f = urlopen(url)
-            o = open(cpu + ".json", "w")
-            o.write(f.read())
-            o.close()
-            f.close()
+            getfile(url, "%s-%s.json" % (cpu, key))
             found += 1
         models.close()
+        getfile(urlpath + "/readme.txt", "readme.txt")
     except URLError as e:
         print "Cannot access event server:", e
     except OSError as e:
@@ -75,8 +80,16 @@ def download_current():
        Returns >0 when a event list is found"""
     return download(get_cpustr())
 
-def eventlist_name():
-    return "%s/.events/%s.json" % (os.getenv("HOME"), get_cpustr())
+def eventlist_name(name=None, key="core"):
+    if not name:
+        name = get_cpustr()
+    cache = os.getenv("XDG_CACHE_HOME")
+    if not cache:
+        cache = os.getenv("HOME")
+        if not cache:
+            return None
+        cache += "/.cache"
+    return "%s/pmu-events/%s-%s.json" % (cache, name, key)
 
 if __name__ == '__main__':
     cpustr = get_cpustr()
@@ -92,4 +105,7 @@ if __name__ == '__main__':
 
     if found == 0:
         print >>sys.stderr, "Nothing found"
-    print "my event list", eventlist_name()
+
+    el = eventlist_name()
+    if os.path.exists(el):
+        print "my event list", el
