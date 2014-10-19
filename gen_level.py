8b21d0224067f05158f7e155ac599932d6ebceb9
Andi Kleen <ak@linux.intel.com>
2014-10-19 12:08:59 -0700

    tl-barplot: Fix level computation

--- gen_level.py
+++ gen_level.py
@@ -2,25 +2,40 @@
 # utility module for other tools
 # the model used must be a superset of all
 import ivb_server_ratios
+import hsw_client_ratios
+import power_metrics
+import re
 
 omap = dict()
+metric = dict()
 
 class Runner:
     def run(self, n):
-        #print "name |%s|" % (n.name), n
+        #print "name |%s|" % (n.name)
         omap[n.name] = n
 
     def metric(self, n):
+        metric[n.name] = n
+        #print "metric |%s|" % (n.name)
         pass
 
 def get_level(name):
     name = name.strip()
-    dot = name.find(".")
+    dot = name.rfind(".")
     if dot >= 0:
-        name = name[dot:]
+        name = name[dot + 1:]
+    oname = name
     if name in omap:
         return omap[name].level
+    name = re.sub(r"([a-z])([A-Z])", r"\1 \2", name, 999)
+    name = name.replace(" ", "_")
+    if name in omap:
+        return omap[name].level
+    if name not in metric and oname not in metric:
+        print "level for %s not found" % (oname)
     return 0
 
 runner = Runner()
 ivb_server_ratios.Setup(runner)
+hsw_client_ratios.Setup(runner)
+power_metrics.Setup(runner)
