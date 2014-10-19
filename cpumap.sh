a0e0f3a6f492870ecc202e84e165b8ba877de461
Andi Kleen <ak@linux.intel.com>
2014-10-19 08:59:02 +0200

    toplev: Fix tl-tester

---
+++ cpumap.sh
@@ -0,0 +1,14 @@
+declare -A cpus
+cpus[nhm-ex]=GenuineIntel-6-2E
+cpus[nhm-ep]=GenuineIntel-6-1E
+cpus[nhm-ep]=GenuineIntel-6-1A
+cpus[wsm-ex]=GenuineIntel-6-2F
+cpus[wsm-sp]=GenuineIntel-6-25
+cpus[wsm-dp]=GenuineIntel-6-2C
+cpus[snb]=GenuineIntel-6-2A
+cpus[jkt]=GenuineIntel-6-2D
+cpus[ivt]=GenuineIntel-6-3E
+cpus[ivb]=GenuineIntel-6-3A
+cpus[hsw]=GenuineIntel-6-45
+cpus[slm]=GenuineIntel-6-37
+cpus[bnl]=GenuineIntel-6-35
