1564ad4293cc934d4b85a2526961311c4530f3f2
Andi Kleen <ak@linux.intel.com>
2014-11-01 07:10:26 -0700

    toplev: linux metrics improvements

--- linux_metrics.py
+++ linux_metrics.py
@@ -9,7 +9,12 @@ import os, sys
 
 class CPU_Utilization:
     name = "CPU utilization"
-    desc = " Number of CPUs used"
+    desc = """
+Number of CPUs used. The top down CPU metrics are only meaningful when a CPU thread is executing.
+The percentage are always relative to the executing time. When the utilization is low the workload
+may actually not be CPU bound, but IO (network, block) IO bound instead. Check the scheduler
+and IO metrics below. Or it may be CPU bound, but not use enough parallelism, if the number
+of CPUs is less than the number of cores."""
     nogroup = True
     subplot = "CPU Utilization"
     unit = "CPUs"
@@ -137,6 +142,11 @@ class NetworkRX:
         self.thresh = self.val > 0
 
 # trace events
+# sched stats? (non perf)
+#   yield percentage
+#   % sched to idle
+#   ttw local vs remote
+#   wait for cpu time
 # sched wakeup
 # sched iowait
 # filemap add to page cache
