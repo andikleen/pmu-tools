523ce33c2a3864b5531eb0d337c04bb7a968771f
Andi Kleen <ak@linux.intel.com>
2014-12-27 20:58:29 -0800

    Reformat and kill spaces in counterdiff.py

--- counterdiff.py
+++ counterdiff.py
@@ -5,25 +5,25 @@
 import sys, os
 
 def run(x):
-	print x
-	os.system(x)
+    print x
+    os.system(x)
 
 for l in sys.stdin:
-	if l.find(",") < 0:
-		continue
-	n = l.strip().split(",")
-        run("perf stat --output l -x, -e %s %s" %
-                    (n[1], " ".join(sys.argv[1:])))
-	f = open("l", "r")
-	for i in f:
-		if i.find(",") < 0:
-			continue
-		j = i.strip().split(",")
-		break
-	f.close()
-	if float(n[0]) > 0:
-		delta = (float(j[0]) - float(n[0]))/float(n[0])
-	else:
-		delta = 0
-	print n[1],j[0],n[0],"%.2f" % (delta * 100.0)
+    if l.find(",") < 0:
+        continue
+    n = l.strip().split(",")
+    run("perf stat --output l -x, -e %s %s" %
+        (n[1], " ".join(sys.argv[1:])))
+    f = open("l", "r")
+    for i in f:
+        if i.find(",") < 0:
+            continue
+        j = i.strip().split(",")
+        break
+    f.close()
+    if float(n[0]) > 0:
+        delta = (float(j[0]) - float(n[0])) / float(n[0])
+    else:
+        delta = 0
+    print n[1], j[0], n[0], "%.2f" % (delta * 100.0)
 
