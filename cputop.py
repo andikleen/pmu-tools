11f32fbb098e8d033937a62a3a09b9ad1c489c69
Andi Kleen <ak@linux.intel.com>
2015-07-30 15:05:55 -0400

    cputop: Fix offline match

--- cputop.py
+++ cputop.py
@@ -14,8 +14,8 @@
 # cputop "thread == 0 and socket == 0"
 # disable all second threads (disable hyper threading)
 # cputop "thread == 1" offline
-# reenable all second threads (reenable hyper threading)
-# cputop "thread == 0" online
+# reenable all offlined cpus
+# cputop offline online
 # Author: Andi Kleen
 import sys, os, re, argparse
 
@@ -30,31 +30,6 @@ def output(p, fmt):
         print fmt % (p,)
     else:
         print p
-   
-base = "/sys/devices/system/cpu/"
-p = {}
-l = os.listdir(base)
-for d in l:
-    m = re.match(r"cpu([0-9]+)", d)
-    if not m:
-        continue
-    proc = int(m.group(1))
-    top = base + d + "/topology"
-    if not os.path.exists(top):
-        if sys.argv[1] == "offline":
-            output(proc)
-        continue
-    socket = numfile(top + "/physical_package_id")
-    core = numfile(top + "/core_id")
-    n = 0
-    while (socket, core, n) in p:
-        n += 1
-    p[(socket, core, n)] = proc
-
-special = {
-    "offline": "echo 0 > /sys/devices/system/cpu/cpu%d/online",
-    "online": "echo 1 > /sys/devices/system/cpu/cpu%d/online",
-}        
 
 ap = argparse.ArgumentParser(description='''
 query cpu topology and print all matching cpu numbers
@@ -68,9 +43,36 @@ ap.add_argument('expr', help='python expression with socket/core/thread')
 ap.add_argument('fmt', help='Output format string with %d, or online/offline', nargs='?')
 args = ap.parse_args()
 
-if args.fmt:
-    if args.fmt in special:
-        args.fmt = special[args.fmt]
+special = {
+    "offline": "echo 0 > /sys/devices/system/cpu/cpu%d/online",
+    "online": "echo 1 > /sys/devices/system/cpu/cpu%d/online",
+}        
+
+if args.fmt in special:
+    args.fmt = special[args.fmt]
+
+base = "/sys/devices/system/cpu/"
+p = {}
+l = os.listdir(base)
+for d in l:
+    m = re.match(r"cpu([0-9]+)", d)
+    if not m:
+        continue
+    proc = int(m.group(1))
+    top = base + d + "/topology"
+    if not os.path.exists(top):
+        if args.expr == "offline":
+            output(proc, args.fmt)
+        continue
+    socket = numfile(top + "/physical_package_id")
+    core = numfile(top + "/core_id")
+    n = 0
+    while (socket, core, n) in p:
+        n += 1
+    p[(socket, core, n)] = proc
+
+if args.expr == "offline":
+    sys.exit(0)
 
 for j in sorted(p.keys()):
     socket, core, thread = j
