0d843155a0f228d7dcfbca2663b851a66b39c3f3
Andi Kleen <ak@linux.intel.com>
2015-10-06 18:56:21 -0700

    toplev: Do not include pebs postfix in sample event names

--- toplev.py
+++ toplev.py
@@ -1555,7 +1555,7 @@ def print_sample(sample_obj, rest):
         if not args.quiet:
 	    print >>sys.stderr, "warning: update kernel to handle sample events:"
 	    print >>sys.stderr, "\n".join(missing)
-    sl = [raw_event(s[0], s[1] + "_" + s[0].replace(".", "_"), period=True) for s in nsamp]
+    sl = [raw_event(s[0], s[1] + "_" + remove_pp(s[0]).replace(".", "_"), period=True) for s in nsamp]
     sl = add_filter(sl)
     sample = ",".join([x for x in sl if x])
     print "Sampling:"
