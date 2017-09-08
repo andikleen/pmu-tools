12c5c8e33b556f7e894bf7fb756de071da0d797e
Andi Kleen <ak@linux.intel.com>
2017-09-08 09:55:36 -0700

    event-rmap: Print edge/cmask/inv/any

--- event-rmap.py
+++ event-rmap.py
@@ -40,21 +40,32 @@ for i in range(0, 8):
                     continue
                 for j in emap.codes.keys():
                     if j == evsel and extra == emap.codes[j].msrvalue:
-                        print j.name, "msr:%x" % (extra)
+                        print j.name, "msr:%x" % (extra),
                 else:
                     print "no exact match for %s, msr %x value %x" % (ev.name,
-                                                                      ev.msr, ev.msrvalue)
+                                                                      ev.msr, ev.msrvalue),
             else:
-                print ev.name
+                print ev.name,
         else:
             name = ""
             for j in emap.codes.keys():
                 if j & 0xff == evsel & 0xff:
                     name += "%s[%x] " % (emap.codes[j].name, j)
             if name:
-                print "[no exact match] " + name
+                print "[no exact match] " + name,
             else:
-                print "r%x" % (evsel)
+                print "r%x" % (evsel),
+        if evsel & EVENTSEL_CMASK:
+            print "cmask=%x" % (evsel >> 24),
+        if evsel & EVENTSEL_EDGE:
+            print "edge=1",
+        if evsel & EVENTSEL_ANY:
+            print "any=1",
+        if evsel & EVENTSEL_INV:
+            print "inv=1",
+        if evsel & EVENTSEL_PC:
+            print "pc=1",
+        print
 if found == 0:
     print "Cannot read any MSRs"
 
