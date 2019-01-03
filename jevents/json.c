9cc2b56c3d6e41651cf49e716f11a434c2dcaa1f
Andi Kleen <ak@linux.intel.com>
2019-01-03 10:48:30 -0800

    jevents: Simplify mmap code

--- jevents/json.c
+++ jevents/json.c
@@ -41,7 +41,6 @@
 
 static char *mapfile(const char *fn, size_t *size)
 {
-	unsigned ps = sysconf(_SC_PAGESIZE);
 	struct stat st;
 	char *map = NULL;
 	int err;
@@ -53,9 +52,7 @@ static char *mapfile(const char *fn, size_t *size)
 	if (err < 0)
 		goto out;
 	*size = st.st_size;
-	map = mmap(NULL,
-		   (st.st_size + ps - 1) & ~(ps -1),
-		   PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, 0);
+	map = mmap(NULL, st.st_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, 0);
 	if (map == (char *)MAP_FAILED)
 		map = NULL;
 out:
@@ -65,8 +62,7 @@ out:
 
 static void unmapfile(char *map, size_t size)
 {
-	unsigned ps = sysconf(_SC_PAGESIZE);
-	munmap(map, (size + ps - 1) & ~(ps - 1));
+	munmap(map, size);
 }
 
 /*
