5f7715ea51c6d6a52c3f1b5c3d41d8f81d1a3439
Andi Kleen <ak@linux.intel.com>
2020-02-28 15:10:01 -0800

    list-events: Port to python3

--- list-events.py
+++ list-events.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python2
 # print all events in a eventmap
+from __future__ import print_function
 import ocperf
 import sys
 emap = ocperf.find_emap()
 if not emap:
     sys.exit("Unknown CPU or cannot find event table")
 for j in sorted(emap.events):
-    print j
+    print(j)
