7a512f9ea2cab8828bf33573e95c0af535e5fac7
Andi Kleen <ak@linux.intel.com>
2020-05-27 21:14:24 -0700

    event-rmap: Port to python3

--- event-rmap.py
+++ event-rmap.py
@@ -2,6 +2,7 @@
 # print currently running events on cpu (default 0)
 # event-rmap [cpu-num]
 # xxx no extra modi for now, racy with multi plexing
+from __future__ import print_function
 import sys
 import msr
 import ocperf
@@ -32,7 +33,7 @@ for i in range(0, 8):
         break
     found += 1
     if evsel & EVENTSEL_ENABLE:
-        print "%d: %016x: " % (i, evsel),
+        print("%d: %016x: " % (i, evsel), end="")
         evsel &= EVMASK
         if evsel in emap.codes:
             ev = emap.codes[evsel]
@@ -40,47 +41,46 @@ for i in range(0, 8):
                 try:
                     extra = msr.readmsr(ev.msr)
                 except OSError:
-                    print "Cannot read extra MSR %x for %s" % (ev.msr, ev.name)
+                    print("Cannot read extra MSR %x for %s" % (ev.msr, ev.name))
                     continue
                 for j in emap.codes.keys():
                     if j == evsel and extra == emap.codes[j].msrvalue:
-                        print j.name, "msr:%x" % (extra),
+                        print(j.name, "msr:%x" % (extra), end="")
                 else:
-                    print "no exact match for %s, msr %x value %x" % (ev.name,
-                                                                      ev.msr, ev.msrvalue),
+                    print("no exact match for %s, msr %x value %x" % (ev.name,
+                                                                      ev.msr, ev.msrvalue), end="")
             else:
-                print ev.name,
+                print(ev.name, end="")
         else:
             name = ""
             for j in emap.codes.keys():
                 if j & 0xff == evsel & 0xff:
                     name += "%s[%x] " % (emap.codes[j].name, j)
             if name:
-                print "[no exact match] " + name,
+                print("[no exact match] " + name, end=" ")
             else:
-                print "r%x" % (evsel),
+                print("r%x" % (evsel), end=" ")
         if evsel & EVENTSEL_CMASK:
-            print "cmask=%x" % (evsel >> 24),
+            print("cmask=%x" % (evsel >> 24), end=" ")
         if evsel & EVENTSEL_EDGE:
-            print "edge=1",
+            print("edge=1", end=" ")
         if evsel & EVENTSEL_ANY:
-            print "any=1",
+            print("any=1", end=" ")
         if evsel & EVENTSEL_INV:
-            print "inv=1",
+            print("inv=1", end=" ")
         if evsel & EVENTSEL_PC:
-            print "pc=1",
+            print("pc=1", end=" ")
         if pebs_enable & (1 << i):
-            print "precise=1",
+            print("precise=1", end=" ")
         print
 if found == 0:
-    print "Cannot read any MSRs"
+    print("Cannot read any MSRs")
 
 try:
     fixed = msr.readmsr(MSR_IA32_FIXED_CTR_CTRL)
 except OSError:
-    print "Cannot read fixed counter MSR"
+    print("Cannot read fixed counter MSR")
     fixed = 0
 for i in range(0, 2):
     if fixed & (1 << (i*4)):
-        print "fixed %d: %s" % (i, fixednames[i])
-        
+        print("fixed %d: %s" % (i, fixednames[i]))
