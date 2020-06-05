37e0e654f0cae6422437f6a5a315b28f1e181a0c
Andi Kleen <ak@linux.intel.com>
2020-06-05 14:39:51 -0700

    Revert "slm-rations: Remove unused/broken slots function"
    This reverts commit 464f0bdb8741738196907565b007ed5e95d279d0.

--- parser/perfdata.py
+++ parser/perfdata.py
@@ -198,15 +198,7 @@ def perf_event_header():
                                 ID_INDEX        = 69,
                                 AUXTRACE_INFO   = 70,
                                 AUXTRACE        = 71,
-                                AUXTRACE_ERROR  = 72,
-                                THREAD_MAP      = 73,
-                                CPU_MAP         = 74,
-                                STAT_CONFIG     = 75,
-                                STAT            = 76,
-                                STAT_ROUND      = 77,
-                                EVENT_UPDATE    = 78,
-                                TIME_CONV       = 79,
-                                COMPRESSED      = 80),
+                                AUXTRACE_ERROR  = 72),
                            Embedded(BitStruct(None,
                                               Padding(1),
                                               Enum(BitField("cpumode", 7),
@@ -288,12 +280,6 @@ def read_format():
                              If(lambda ctx: read_flags(ctx).id,
                                 UNInt64("id2")))))
 
-def time_conv():
-    return Struct("time_conv",
-                  UNInt64("time_shift"),
-                  UNInt64("time_mult"),
-                  UNInt64("time_zero"))
-
 def perf_event():
     return Struct("perf_event",
                   Anchor("start"),
@@ -323,9 +309,7 @@ def perf_event():
                                                       SNInt32("tid"),
                                                       read_format(),
                                                       sample_id())),
-                              "SAMPLE": event(),
-                              # some missing cases
-                              "TIME_CONV": time_conv(),
+                              "SAMPLE": event()
                            }),
                         Anchor("end"),
                         Padding(lambda ctx:
