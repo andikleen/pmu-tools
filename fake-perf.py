50b52908c8d843553fbf1a5f0412e243940083f1
Andi Kleen <ak@linux.intel.com>
2020-06-05 11:06:53 -0700

    Add fake-perf.py for testing

---
+++ fake-perf.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# mock perf for limited test environments
+from __future__ import print_function
+import sys
+
+out = sys.stderr
+av = sys.argv
+if av[-1] == "true":
+    sys.exit(0)
+j = 1
+process = True
+while j < len(sys.argv):
+    if av[j] == "--version":
+        print("perf version 5.6.8", end='')
+        break
+    elif av[j] == "-o" and process:
+        j += 1
+        out = open(av[j], "w")
+    elif av[j] == "--":
+        process = False
+    print(av[j], file=out, end=' ')
+    j += 1
+out.write("\n")
+
