e14447637fe03f6be8435e893bbc124e230bc6b8
Andi Kleen <andi@firstfloor.org>
2020-11-21 11:16:27 -0800

    toplev: Add sanity check for all 0 measurements

--- toplev.py
+++ toplev.py
@@ -1578,6 +1578,8 @@ def print_and_split_keys(runner, res, rev, valstats, out, interval, env):
         print_keys(runner, res, rev, valstats, out, interval, env, mode)
 
 def print_and_sum_keys(runner, res, rev, valstats, out, interval, env):
+    if res and all([sum(res[k]) == 0.0 for k in res.keys()]):
+        sys.exit("All measured values 0. perf broken?")
     if args.interval and interval is None:
         interval = float('nan')
     if runner.summary:
