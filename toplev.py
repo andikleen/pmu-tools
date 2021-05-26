139af64479d73a1643460b221358189386d4eb38
Andi Kleen <andi@firstfloor.org>
2021-05-26 07:40:56 -0700

    toplev: Use defaultdict for runner_split

--- toplev.py
+++ toplev.py
@@ -1792,8 +1792,10 @@ def execute_no_multiplex(runner_list, allowed_threads, out, rest, summary):
 
 def runner_split(runner_list, res):
     for r in runner_list:
-        if r.cpu_list and len(runner_list) > 1:
-            yield r, { "%d" % k: res["%d" % k] for k in r.cpu_list }
+        if r.cpu_list: # and len(runner_list) > 1:
+            d = defaultdict(list)
+            d.update({ "%d" % k: res["%d" % k] for k in r.cpu_list })
+            yield r, d
         else:
             yield r, res
 
