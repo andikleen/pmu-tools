f8aec35d8ee41c83128848784f6511465c706d98
Andi Kleen <ak@linux.intel.com>
2022-01-19 21:55:42 -0800

    toplev: Only suggest --run-sample if not specified

--- toplev.py
+++ toplev.py
@@ -3526,7 +3526,8 @@ def suggest_desc(runner):
         "s" if len(printer.bottlenecks) > 1 else "",
         (" for " + runner.pmu.replace("cpu_", "")) if runner.pmu and runner.pmu != "cpu" else ""),
         file=sys.stderr)
-    print_once("Add --run-sample to find locations")
+    if not args.run_sample:
+        print_once("Add --run-sample to find locations")
 
 def sysctl(name):
     try:
