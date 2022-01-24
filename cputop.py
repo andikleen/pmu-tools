fe335583596ec14d4ce965d810488382a7c8782a
Andi Kleen <ak@linux.intel.com>
2022-01-24 15:44:22 -0800

    cputop: Support shortcuts for core/atom

--- cputop.py
+++ cputop.py
@@ -4,6 +4,7 @@
 # query is a python expression, using variables:
 # socket, core, thread, type, cpu
 # or "offline" to query all offline cpus
+# or "atom" or "core" to select core types
 # type can be "atom" or "core"
 # cpu is the cpu number
 # format is a printf format with %d
@@ -70,6 +71,11 @@ ap.add_argument('expr', help='python expression with socket/core/thread')
 ap.add_argument('fmt', help='Output format string with %%d, or online/offline', nargs='?')
 args = ap.parse_args()
 
+if args.expr == "atom":
+    args.expr = 'type == "atom"'
+if args.expr == "core":
+    args.expr = 'type == "core"'
+
 special = {
     "offline": "echo 0 > /sys/devices/system/cpu/cpu%d/online",
     "online": "echo 1 > /sys/devices/system/cpu/cpu%d/online",
@@ -80,15 +86,15 @@ if args.fmt in special:
 
 types = dict()
 for fn in glob.glob("/sys/devices/cpu_*/cpus"):
-    type = os.path.basename(fn.replace("/cpus", "")).replace("cpu_","")
+    typ = os.path.basename(fn.replace("/cpus", "")).replace("cpu_","")
     cpus = open(fn).read()
     for j in cpus.split(","):
         m = re.match(r'(\d+)(-\d+)?', j)
         if m.group(2):
             for k in range(int(m.group(1)), int(m.group(2)[1:])+1):
-                types[k] = type
+                types[k] = typ
         else:
-            types[int(m.group(1))] = type
+            types[int(m.group(1))] = typ
 
 base = "/sys/devices/system/cpu/"
 p = {}
