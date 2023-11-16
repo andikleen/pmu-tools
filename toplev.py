ba956ab9381d076ed012de4f8de4b47e96fb51a8
Andi Kleen <ak@linux.intel.com>
2023-11-16 14:39:56 -0800

    toplev: Fix eventmap error reporting

--- toplev.py
+++ toplev.py
@@ -4028,10 +4028,10 @@ def runner_emaps():
         runner.ectx.emap = ocperf.find_emap(pmu=runner.pmu if runner.pmu else "cpu")
         runner.printer.emap = ectx.emap
         if not runner.ectx.emap:
-            os.environ["OCVERBOSE"] = "1"
+            ocperf.ocverbose = True
             ocperf.find_emap()
             sys.exit("Unknown CPU or CPU event map not found (EVENTMAP:%s, model:%d)" %
-                     (os.environ["EVENTMAP"] if "EVENTMAP" in os.environ else "", cpu.mode))
+                     (os.environ["EVENTMAP"] if "EVENTMAP" in os.environ else "?", cpu.model))
         if version:
             version += ", "
         version += model_setup(runner, cpu.cpu)
